8c86446e9b589ebabf3bd971b60a9e1091781f1b
Rodrigo Perez <rodrigo.perezluna@hotmail.com>
2018-11-24 21:55:56 -0600

    add handler client

--- src/main/java/com/ws/async/handler/AsyncClientHandler.java
+++ src/main/java/com/ws/async/handler/AsyncClientHandler.java
@@ -6,6 +6,10 @@
 package com.ws.async.handler;
 
 import com.ws.async.GetMovieResponse;
+import com.ws.async.Movie;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.ws.AsyncHandler;
 import javax.xml.ws.Response;
 
@@ -15,8 +19,20 @@ import javax.xml.ws.Response;
  */
 public class AsyncClientHandler implements AsyncHandler<GetMovieResponse>{
 
+    private GetMovieResponse response;
+    
     @Override
     public void handleResponse(Response<GetMovieResponse> res) {
+        try {
+            response = res.get();
+            Movie m = response.getReturn();
+            System.out.println("Result id from hanlder = "+m.getId());
+            System.out.println("Result release date from handler = "+m.getReleaseDate());
+        } catch (InterruptedException ex) {
+            Logger.getLogger(AsyncClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ExecutionException ex) {
+            Logger.getLogger(AsyncClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
     
